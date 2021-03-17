ed14f292f8953f0ca666508e1e1be9e84279cce6
Andrew G <vardantexcode@gmail.com>
2021-03-17 12:49:44 -0400

    Update

---
+++ CalculatorDEMO2/app/src/main/java/com/example/calculatordemo/MyActivity.java
@@ -0,0 +1,33 @@
+package com.example.calculatordemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextWatcher;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.TextView;
+
+public class MyActivity extends Activity {
+
+    //DATA MODEL FOR SHIP ITEM
+    private ShipItem shipItem;
+
+    //View Objects for layout UI Reference
+    private EditText weightET;
+    private TextView baseCostTV;
+    private TextView addedCostTV;
+    private TextView totalCostTV;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    //Task 1: SET Activity Conent
+        
+
+    }
+
+
+
+}
